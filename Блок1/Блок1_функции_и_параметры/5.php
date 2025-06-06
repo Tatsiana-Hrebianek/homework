58ced73ee00f809e53a926d0d3277906766bfca1
Tatsiana Hrebianek <tatsiana.hrebianek@gmail.com>
2025-06-06 14:59:52 +0300

    Test commit for PR

--- Блок1/Блок1_функции_и_параметры/5.php
+++ Блок1/Блок1_функции_и_параметры/5.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types=1); //declaration
 
 function generatePassword(int $length = 8, bool $includeNumbers = true, bool $includeSpecialChars = false)
 {
