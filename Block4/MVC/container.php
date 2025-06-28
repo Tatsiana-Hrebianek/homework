dd9aa92efc28ed5e22b9623d34560a8fc786f270
Tatsiana Hrebianek <tatsiana.hrebianek@gmail.com>
2025-06-28 16:35:12 +0300

    Использование провайдера

--- Block4/MVC/container.php
+++ Block4/MVC/container.php
@@ -4,7 +4,9 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use DI\ContainerBuilder;
 use App\Repositories\UserRepository;
+use App\Interfaces\UserRepositoryInterface;
 use App\Services\UserService;
+use App\Providers\UserServiceProvider;
 
 $containerBuilder = new ContainerBuilder();
 
@@ -12,19 +14,20 @@ $containerBuilder->addDefinitions([
     PDO::class => function() {
         return new PDO("mysql:host=localhost;dbname=test_database;charset=utf8mb4", "tatsiana", "");
     },
-    UserRepository::class =>function($c) {
-        return new UserRepository($c->get(PDO::class));
-    },
-    UserService::class => function($c) {
-        return new UserService($c->get(UserRepository::class));
-    },
+    // UserRepositoryInterface::class =>function($c) {
+    //     return new UserRepository($c->get(PDO::class));
+    // },
+    // UserService::class => function($c) {
+    //     return new UserService($c->get(UserRepositoryInterface::class));
+    // },
 ]);
 
 $container = $containerBuilder->build();
 
+//Регистрируем провайдер
+UserServiceProvider::register($container);
+
 return $container;
 
-// $container->set(Database::class, function(){
-//     return new Database('localhost', 'test_database', 'tatsiana');
-// });
+
 
