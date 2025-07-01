056fb3892f7c20ce9e8126d790cb28b2d2b8c817
Tatsiana Hrebianek <tatsiana.hrebianek@gmail.com>
2025-07-01 21:42:58 +0300

    json changes

--- Block4/SwooleServer/src/server.php
+++ Block4/SwooleServer/src/server.php
@@ -6,6 +6,17 @@ use Swoole\Http\Response;
 
 $server = new Server("127.0.0.1", 9501);
 
+//Таймер при старте сервера
+$server->on("start", function (Server $server) {
+    echo "Сервер запущен: http://127.0.0.1:9501\n";
+
+    //Асинхронный таймер: каждую 5-ю секунду выполняем callback
+    Swoole\Timer::tick(5000, function(){
+        echo " Таймер сработал: " . date('Y-m-d H:i:s') . "\n";
+    });
+});
+
+//Обработка HTTP-запросов
 $server->on("request", function(Request $request, Response $response) {
     $response->header("Content-Type", "text/plain; charset=utf-8");
     $response->end("Привет от Swoole!");
