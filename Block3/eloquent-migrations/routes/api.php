6d0830ee17773524de0b150c42fee6543336973d
Tatsiana Hrebianek <tatsiana.hrebianek@gmail.com>
2025-07-02 19:58:52 +0300

    Создание REST API (GET-запрос), обработка POST-запроса, обновление данных через (PUT/PATCH), удаление данных (DELETE)

---
+++ Block3/eloquent-migrations/routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+Route::post('/users', [UserController::class, 'store']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+Route::get('/ping', function () {
+    return 'pong';
+});
